c8011032b799f50adef18114d66b8472a02e3a1e
Jennifer-Astrid <jenniferaccamargo@gmail.com>
2024-06-03 12:37:01 -0300

    Commit do desafio do Felipão

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("oi")
+let heroName = "Trid"
+let heroExperience = 9500
+
+
+if (heroExperience <= 1000) {
+    experience = "Ferro"
+} else if (heroExperience >= 1001 && heroExperience < 2000){
+    experience = "Bronze"
+} else if (heroExperience >= 2001 && heroExperience < 3000){
+    experience = "Prata"
+} else if (heroExperience >= 3001 && heroExperience < 5000){
+    experience = "Ouro"
+} else if (heroExperience >= 5001 && heroExperience < 6000){
+    experience = "Platina"
+} else if (heroExperience >= 6001 && heroExperience < 7000){
+    experience = "Diamante"
+} else if (heroExperience >= 7001 && heroExperience < 8000){
+    experience = "Ascendente"
+} else if (heroExperience >= 8001 && heroExperience < 9000){
+    experience = "Imortal"
+} else {
+    experience = "Radiante"
+};
+
+
+console.log("O Herói de nome " + heroName + " está no nível " + experience + "!")
